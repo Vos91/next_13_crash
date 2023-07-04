5ecfb3e77106cf36d664141b44815cbf752ec5f2
Jasper Vos <jasper@mediasolutions>
2023-07-04 16:48:43 +0200

    first commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+export const metadata = {
+  title: 'About Vos',
+};
+
 const AboutPage = () => {
   return (
     <div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,32 @@
-import Link from 'next/link';
+'use client';
+import { useState, useEffect } from 'react';
+import LoadingPage from './loading';
+import Courses from './components/Courses';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) return <LoadingPage />;
+
   return (
-    <div>
+    <>
       <h1>Welcome To Vos</h1>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-        <li>
-          <Link href='/about/team'>Team</Link>
-        </li>
-      </ul>
-    </div>
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
+    </>
   );
 };
 
