79cb4f1d3551fae583a4c1ad7401cd8aa5d850b6
Jasper Vos <jasper@mediasolutions>
2023-07-04 16:48:01 +0200

    Crash course

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Vos</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque ad
+        exercitationem mollitia quod fugit quibusdam veniam, animi officiis,
+        illo nam quo, pariatur vero totam deserunt reiciendis delectus minima
+        sapiente. Porro quas necessitatibus eos placeat delectus officia
+        dolores, vel quasi voluptate totam corporis quidem dolor corrupti ea et
+        molestias quia deleniti.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure vero
+        aliquam sit consequuntur nisi fuga quas, necessitatibus quaerat repellat
+        modi inventore magnam illum? Ipsam dolorem similique velit. Tempore
+        officiis aut dolores culpa vero ipsam odit deserunt corrupti commodi
+        porro, maiores ab voluptas consequatur molestias, deleniti eligendi
+        dolorem odio explicabo!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
